bb288f5982471b05c24306cb8737052f5dfd29f6
Genesis Gir <daintygenesis@gmail.com>
2021-09-05 08:25:45 -0700

    [NEW]

--- csides/RoboticSyndrome/RoboticSyndrome.py
+++ csides/RoboticSyndrome/RoboticSyndrome.py
@@ -4,7 +4,8 @@
 Welcome to the genesis gir lesson tutorials Volume 1! Robotic Syndrome takes the user to a room with a manic robot 
 that makes them guess they right number 10 times if not user will end up perishing from the gigantic lazer that the robot
 has in store for them! Guess the right number to survive if you guessed wrong you will know and find yourself out the
-room filled with mechanical parts from the future met for your demiseThanks for downloading~ 
+room filled with mechanical parts from the future met for your demise. Being locked inside of a room with no way out
+like in life you only get one chance to live and it's the same for Robotic Syndrome Thanks for downloading~ 
 ⼕ㄖᗪ🝗ᗪ & 山尺讠セセ🝗𝓝 ⻏丫 Ꮆ🝗𝓝🝗丂讠丂 Ꮆ讠尺
 """
 # This program takes the user on a hostage situation with a robot that has user trapped in a room met for death!
@@ -15,124 +16,129 @@ import sys
 # Character variable/constants
 robo = 'Machine TM97:'
 
-print()
-print('-Your in a white room and something opens the door slowly -')
-print()
-print(f'{robo}Wake up! BUZZ Brrr')
-print('(press enter to wake up)')
+print()#(ELOC)
+print()#(ELOC)
+print('                                        █▀█ █▀█ █▄▄ █▀█ ▀█▀ █ █▀▀   █▀ █▄█ █▄░█ █▀▄ █▀█ █▀█ █▀▄▀█ █▀▀')
+print('                                        █▀▄ █▄█ █▄█ █▄█ ░█░ █ █▄▄   ▄█ ░█░ █░▀█ █▄▀ █▀▄ █▄█ █░▀░█ ██▄')
+print('                                        written & coded by genesis gir')
+print()#(ELOC)
+print('-Your in a white room and something opens the door slowly -')# (SOD) Sense of direction by () or by story
+print()#(ELOC)
+print(f'{robo}Wake up! BUZZ Brrr')#speech dialog from (Machine TM97) uses print function
+print('(press enter to wake up)')#prompts user to press enter to wake up w/print
 input()
-print('-The robotic dictator splashes water against your face and you see a tall mechanical strong humanoid-')
-print('(press enter)')
+print('-The robotic dictator splashes water against your face and you see a tall mechanical strong humanoid-')# (SOA) Sense of action by (Machine TM97) or by story
+print('(press enter)')#prompts user to press enter w/print
 input()
-print(f'{robo} Whats the number Im thinking of? Guess its between 1-10')
-print()
-print('-The robot leans over the table clenching his fists in such a anger manner and the oil dripping out of his eyes')
-print('its clear that your in trouble and need to give the robot his desired answer or who knows what it will..do to you')
-print('(guess a number between 1 and 10)')
+print(f'{robo} Whats the number Im thinking of? Guess its between 1-10')#speech dialog from (Machine TM97) uses print function
+print()#(ELOC)
+print('-The robot leans over the table clenching his fists in such a angry manner and the oil dripping out of his eyes')# (SOA) Sense of action by (Machine TM97) or by story
+print('its clear that your in trouble and need to give the robot his desired answer or who knows what it will..do to you')#(SOA) Sense of action () continues
+print('(guess a number between 1 and 10 than press enter)') # prompts user to guess a number w/print
 
-guess = input()
+guess = input() # guess variable stores users standard input
 
 if guess =='6': # answer to the manic robot is '6'
-    print(f'{robo} Yes. .Yes BRRr')
-    print('(press enter)')
-    input()
-    print(f'{robo} The number was 6. . Lets play again?')
-    response = input('(y/n?)')
+    print(f'{robo} Yes. .Yes BRRr')#speech dialog from (Machine TM97) uses print function
+    print('(press enter)')#prompts user to press enter w/print
+    input()# T/I
+    print(f'{robo} The number was 6. . Lets play again?')#speech dialog from (Machine TM97) uses print function
+    response = input('(y/n?)') # response variable will contain users standard input w/input()
     
     if response == 'y': # answer to the manic robot is 'y'
-        print(f'{robo} Great lets play again shall we')
-        print('(press enter to play again)')
-        input()
-        print(f'{robo} I wan-ntt you to guess the number inside my data mainframe between 1-10')
-        print()
-        print('-The robots voice gets deeper and less angry but you can tell the voice has calmed-')
-        print('(guess a number between 1 and 10)')
+        print(f'{robo} Great lets play again shall we')#speech dialog from (Machine TM97) uses print function
+        print('(press enter to play again)')#prompts user to press enter to play again w/print
+        input()# T/I
+        print(f'{robo} I wan-ntt you to guess the number inside my data mainframe between 1-10')#speech dialog from (Machine TM97) uses print function
+        print()#(ELOC)
+        print('-The robots voice gets deeper and less angry but you can tell the voice has calmed-')# (SOA) Sense of action by (Machine TM97) or by story
+        print('(guess a number between 1 and 10 than press enter)')# prompts user to guess a number w/print
         
-        guess = input()
+        guess = input() # guess variable stores users standard input
         
         if guess == '8': # answer to the manic robot is '8'
-            print(f'{robo} Did not know humans were smart. yes correct!')
-            print('(press enter)')
-            input()
-            print(f'{robo} Lets play the game again cause your brain intrigues me.')
+            print(f'{robo} Did not know humans were smart. yes correct!')#speech dialog from (Machine TM97) uses print function
+            print('(press enter)')#prompts user to press enter w/print
+            input()# T/I
+            print(f'{robo} Lets play the game again cause your brain intrigues me.')#speech dialog from (Machine TM97) uses print function
             print('(press enter to play the game again)')
-            input()
-            print('-You sit and think to yourself how unjustified and creepy this game is-')
-            print('(press enter)')
-            input()
-            print(f'{robo} Okay! BRRrrrr Buuzzzz')
-            print('(press enter to stare)')
-            input()
-            print(f'{robo} What number am I thinking between 1-6?')
-            print('(guess a number between 1-6 than press enter)')
+            input()# T/I (takes input) waits for user to type something onto the keyboard than press enter key!
+            print('-You sit and think to yourself how unjustified and creepy this game is-')# (SOA) Sense of action by (User) or by story
+            print('(press enter)')#prompts user to press enter w/print
+            input()# T/I
+            print(f'{robo} Okay! BRRrrrr Buuzzzz')#speech dialog from (Machine TM97) uses print function
+            print('(press enter to stare)')#prompts user to press enter to stare w/print
+            input()# T/I
+            print(f'{robo} What number am I thinking between 1-6?')#speech dialog from (Machine TM97) uses print function
+            print('(guess a number between 1-6 than press enter)')# prompts user to guess a number w/print
             
-            guess = input()
+            guess = input() # guess variable stores users standard input
             
             if guess == '2': # answer to the manic robot is '2'
-                print(f'{robo} Yes yes! it was 2! Brrr Buzz')
-                print('(press enter)')
-                input()
-                print('-As the blood flows faster and faster the strong feeling that this robot might want to kill you as')
-                print('you notice the humanoid is holding a huge knife that can contract with their robotic parts and suit-')
-                print('(press enter)')
-                input()
-                print(f'{robo} Lets play one more time this one will be hard! Guess the number Im thinking of its between')
-                print('1-15')
-                print('(press enter)')
-                input()
-                print('-He bumped up the odds to my life. . -')
-                print('(press enter)')
-                input()
-                print('(guess a number between 1-15 than press enter)')
+                print(f'{robo} Yes yes! it was 2! Brrr Buzz')#speech dialog from (Machine TM97) uses print function
+                print('(press enter)')#prompts user to press enter w/print
+                input()# T/I
+                print('-As the blood flows faster and faster the strong feeling that this robot might want to kill you as')# (SOA) Sense of action by (User) or by story
+                print('you notice the humanoid is holding a huge knife that can contract with their robotic parts and suit-')# (SOA) Sense of action by (User) continues or by story
+                print('(press enter)')#prompts user to press enter w/print
+                input()# T/I
+                print(f'{robo} Lets play one more time this one will be hard! Guess the number Im thinking of its between')#speech dialog from (Machine TM97) uses print function
+                print('1-15')#speech dialog from (Machine TM97) continues uses print function
+                print('(press enter)')#prompts user to press enter w/print
+                input()# T/I
+                print('-He bumped up the odds to my life. . -')#speech dialog from (User) uses print function
+                print('(press enter)')#prompts user to press enter w/print
+                input()# T/I
+                print('(guess a number between 1-15 than press enter)')# prompts user to guess a number w/print
                 
-                guess = input()
+                guess = input() # guess variable stores users standard input
                 
                 if guess == '11': # answer to the manic robot is '11'
-                    print(f'{robo} 11 was the right answer! you...')
-                    print('(press enter)')
-                    input()
-                    print(f'{robo} win! you may leave this room.')
-                    print('(press enter to exit the room safe and sound)')
-                    input()
-                    print('(YOU WIN!)')
-                    sys.exit()
+                    print(f'{robo} 11 was the right answer! you...')#speech dialog from (Machine TM97) uses print function
+                    print('(press enter)')#prompts user to press enter w/print
+                    input()# T/I
+                    print(f'{robo} win! you may leave this room.')#speech dialog from (Machine TM97) uses print function
+                    print('(press enter to exit the room safe and sound)')#prompts user to press enter to exit the room w/print
+                    input()# T/I
+                    print('(YOU WIN!)')#lets user know that they have won!
+                    sys.exit() # A Module that terminates the program early
                 else:
-                    print(f'{robo} You will die and it was your last chance too..')
-                    print('(press enter to brace yourself for death)')
-                    input()
-                    print('-Stabby Stab Stab -')
-                    print('(GAME OVER)')
-                    sys.exit()
+                    print(f'{robo} You will die and it was your last chance too..')#speech dialog from (Machine TM97) uses print function
+                    print('(press enter to brace yourself for death)')#prompts user to press enter to brace themselves w/print
+                    input() # takes users input from user and waits for them to press enter to key to resume! w/input()
+                    print('-Stabby Stab Stab -') # User is being stabbed! uses w/print()
+                    print('(GAME OVER)') # lets user know that game is over and they lost!
+                    sys.exit() # A Module that terminates the program early
                     
                 
                 
                 
                 
             else:
-                print('-The robot gets up and starts to choke you to death!-')
-                print('(GAME OVER)')
-                sys.exit()
+                print('-The robot gets up and starts to choke you to death!-')# (SOA) Sense of action by (Machine TM97) or by story
+                print('(GAME OVER)') # lets user know that game is over and they lost!
+                sys.exit() # A Module that terminates the program early
             
             
             
         else:
-            print('-The robot stands up-')
-            print('(press enter)')
-            input()
-            print('-You have been stabbed and are bleeding to your death-')
-            print('(GAME OVER)')
-            sys.exit()
+            print('-The robot stands up-')# (SOA) Sense of action by (Machine TM97) or by story
+            print('(press enter)')#prompts user to press enter w/print
+            input()# T/I
+            print('-You have been stabbed and are bleeding to your death-') # shows that user is dying w/print()
+            print('(GAME OVER)') # lets user know that game is over and they lost!
+            sys.exit() # A Module that terminates the program early
         
         
         
     elif response == 'n':
-        print('-The robot picks you up and rips you to shreds..you bleed out-')
-        print('(GAME OVER)')
-        sys.exit()
+        print('-The robot picks you up and rips you to shreds..you bleed out-')# (SOA) Sense of action by (Machine TM97) or by story
+        print('(GAME OVER)') # lets user know that game is over and they lost!
+        sys.exit() # A Module that terminates the program early
 else:
-    print('-The robot starts to tear you to shreds and you die in a pool of your own blood-')
-    print('(GAME OVER)')
-    sys.exit()
+    print('-The robot starts to tear you to shreds and you die in a pool of your own blood-')# (SOA) Sense of action by (Machine TM97) or by story
+    print('(GAME OVER)') # lets user know that game is over and they lost!
+    sys.exit() # A Module that terminates the program early
     
 
 
