5c6583beba1a25f7f31d7669c1b89778b6775cc8
genesisgir <daintygenesis@gmail.com>
2022-04-04 09:00:44 -0400

    [NEW]

---
+++ csides/Laxcity/README.md
@@ -0,0 +1,34 @@
+# README.md
+
+# Laxcity
+Heres a program i created using the webbrowser function and uses the function call to open up a web page using modules!
+the art of coding can be really fun and amazing when things come together, I put the url of my favorite laxcity track by far
+GRANTED by Laxcity make sure to follow him on soundcloud!
+
+Laxcity: https://soundcloud.com/laxcity (B-SIDES) / https://soundcloud.com/laxcitymusic (MAIN)
+
+║░█░█░║░█░█░█░║░█░█░║
+║░█░█░║░█░█░█░║░█░█░║
+║░║░║░║░║░║░║░║░║░║░║
+╚═╩═╩═╩═╩═╩═╩═╩═╩═╩═╝
+            Tips: The webbrowser module allows you to open web pages using webbrowser('insert URL')
+Program created by genesisgir
+
+![Screenshot 2021-09-05 05-16-07](https://user-images.githubusercontent.com/87259615/132131776-e08d5983-5fb7-45fd-b15b-ccf84c708afe.png)
+
+# Learn , Discover , Explore!
+In this .py you can learn alot from what I did and discover methods I use to make things work! This is very Novice level friendly
+and if you find yourself lost just make sure to look at the comments next to the lines of code to see what each line does and why 
+I added those functions and how they are being implmented. again this is very user friendly and very easy to understand I made
+these projects so someone new to coding can learn and develop skills like I have to make their own programs and have somewhere to
+start in the world of coding!
+
+# Twitch
+I also stream my progress on Twitch and store the broadcasts so subscribers can rewatch and see how i made them from scratch! I stream for hours
+at a time and make sure to stop by and ask me any questions or , Chat , watch my progress!
+
+# screenshots
+
+
+
+
---
+++ csides/Laxcity/laxcity.py
@@ -0,0 +1,50 @@
+"""
+░██████╗░███████╗███╗░░██╗███████╗░██████╗██╗░██████╗░██████╗░██╗██████╗░
+██╔════╝░██╔════╝████╗░██║██╔════╝██╔════╝██║██╔════╝██╔════╝░██║██╔══██╗
+██║░░██╗░█████╗░░██╔██╗██║█████╗░░╚█████╗░██║╚█████╗░██║░░██╗░██║██████╔╝
+██║░░╚██╗██╔══╝░░██║╚████║██╔══╝░░░╚═══██╗██║░╚═══██╗██║░░╚██╗██║██╔══██╗
+╚██████╔╝███████╗██║░╚███║███████╗██████╔╝██║██████╔╝╚██████╔╝██║██║░░██║
+░╚═════╝░╚══════╝╚═╝░░╚══╝╚══════╝╚═════╝░╚═╝╚═════╝░░╚═════╝░╚═╝╚═╝░░╚═╝
+
+
+█░░ ▄▀█ ▀▄▀ █▀▀ █ ▀█▀ █▄█
+█▄▄ █▀█ █░█ █▄▄ █ ░█░ ░█░
+
+Heres a program i created using the webbrowser function and uses the function call to open up a web page using modules!
+the art of coding can be really fun and amazing when things come together, I put the url of my favorite laxcity track by far
+GRANTED by Laxcity make sure to follow him on soundcloud!
+
+Laxcity: https://soundcloud.com/laxcity (B-SIDES) / https://soundcloud.com/laxcitymusic (MAIN)
+
+║░█░█░║░█░█░█░║░█░█░║
+║░█░█░║░█░█░█░║░█░█░║
+║░║░║░║░║░║░║░║░║░║░║
+╚═╩═╩═╩═╩═╩═╩═╩═╩═╩═╝
+            Tips: The webbrowser module allows you to open web pages using webbrowser('insert URL')
+Program created by genesisgir
+"""
+#Importing modules with the import statement
+import webbrowser,time,sys,random
+
+# genesis gir constant
+genesis = 'Genesis:'
+
+#creating the function call laxcity with the def statement!
+def laxcity(songname):
+    print(genesis + 'Here is a song I really like from laxcity!')
+    time.sleep(2)
+    print(genesis + 'You will be directed to soundcloud!')
+    time.sleep(2)
+    print('Loading webbrowser...')
+    time.sleep(2)
+    print('getting things together')
+    time.sleep(2)
+    print('Enjoy!')
+    time.sleep(1)
+    webbrowser.open('https://soundcloud.com/laxcity/granted')
+    print(songname + ' started playing!')
+    
+laxcity('Laxcity - GRANTED')
+
+
+sys.exit()
